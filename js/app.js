e51cdd292cbf1739f84fba8fe88453fa8512f358
Frozen <kiril.kohan@gmail.com>
2019-01-17 10:48:38 +0200

    Added UI controller logic
    Updated relationships between modules.  Added DOMStrings and DOM objects for escaping hardcoded strings.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
 // Budget controller
-var budgetController = (function(){
-// Some cide
+var budgetController = (function() {
+  // Some code
 })();
 
 // UI Controller
 var UIController = (function() {
-  // Some cide
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 // Global app controller
 var controller = (function(budgetCtrl, UIctrl) {
-  document.querySelector('.add__btn').addEventListener('click', function() {
-    
+
+  var DOM = UIctrl.getDOMstrings();
+
+  var ctrlAddItem = function() {
     // TODO: 1. Get the field input data
+    var input = UIController.getInput();
+    console.log(input);
 
     // TODO: 2. Add the item to the budget controller
 
@@ -21,6 +43,15 @@ var controller = (function(budgetCtrl, UIctrl) {
     // TODO: 4. Calculate budget
 
     //// TODO: 5. display the budget on the UI
+    
+  };
 
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
   });
+
 })(budgetController, UIController);
