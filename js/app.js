2b029d222bcd064fc849fa1468739ee361edc157
Frozen <kiril.kohan@gmail.com>
2019-01-17 09:39:38 +0200

    Initilize the basic structure of project
    Grouped files by folders and separating app.js for controllers

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Budget controller
+var budgetController = (function(){
+// Some cide
+})();
+
+// UI Controller
+var UIController = (function() {
+  // Some cide
+})();
+
+// Global app controller
+var controller = (function(budgetCtrl, UIctrl) {
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    
+    // TODO: 1. Get the field input data
+
+    // TODO: 2. Add the item to the budget controller
+
+    // TODO: 3. Add the item to the UI
+
+    // TODO: 4. Calculate budget
+
+    //// TODO: 5. display the budget on the UI
+
+  });
+})(budgetController, UIController);
