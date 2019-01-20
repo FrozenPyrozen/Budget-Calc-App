ef51227811752e5e144148abd158774c380564bf
Frozen <kiril.kohan@gmail.com>
2019-01-20 21:29:17 +0200

    Update app.js
    Added display budfet logic in ui controller. And inital set to 0

--- js/app.js
+++ js/app.js
@@ -97,7 +97,11 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expenseContainer: '.expenses__list'
+    expenseContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expenseLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   // Public methods
@@ -146,6 +150,19 @@ var UIController = (function() {
       // Create focus on 1-st element(inputDescription)
       fieldsArr[0].focus();
     },
+
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+
+      if(obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -176,7 +193,7 @@ var controller = (function(budgetCtrl, UIctrl) {
     var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-    console.log(budget);
+    UIctrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function() {
@@ -204,6 +221,12 @@ var controller = (function(budgetCtrl, UIctrl) {
   return {
     init: function() {
       console.log('Application has started.');
+      UIctrl.displayBudget( {
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setUpEventListeners();
     }
   };
